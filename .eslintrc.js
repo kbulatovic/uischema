363f12259970d61752d340193d59c26187a52e32
konstantinB1 <konstantinbulatovic1@gmail.com>
2023-04-10 23:57:21 +0200

    Add env, and source files

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    root: true,
+
+    settings: {
+        'import/resolver': {
+            node: {
+                moduleDirectory: ['node_modules', './'],
+                extensions: ['.js', '.ts'],
+            },
+        },
+    },
+
+    parser: '@typescript-eslint/parser',
+
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+    },
+
+    overrides: [
+        {
+            files: ['*.ts'],
+            parserOptions: {
+                project: ['./tsconfig.json'],
+            },
+        },
+    ],
+
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+        jest: true,
+    },
+
+    plugins: ['prettier', '@typescript-eslint'],
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function a() {
+    return 123;
+}
+
+export default a;
